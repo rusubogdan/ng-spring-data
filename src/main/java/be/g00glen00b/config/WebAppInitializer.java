a98d5f0d5759ee2eb1e75efbfc0a150623bdebfa
Michaël van de Giessen <mg@smile.nl>
2015-09-24 11:35:24 +0200

    formatting/imports

--- src/main/java/be/g00glen00b/config/WebAppInitializer.java
+++ src/main/java/be/g00glen00b/config/WebAppInitializer.java
@@ -2,38 +2,39 @@ package be.g00glen00b.config;
 
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-  @Override
-  protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-    registration.setInitParameter("dispatchOptionsRequest", "true");
-    registration.setAsyncSupported(true);
-  }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setInitParameter("dispatchOptionsRequest", "true");
+        registration.setAsyncSupported(true);
+    }
 
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return new Class<?>[] { AppConfig.class };
-  }
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { AppConfig.class };
+    }
 
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return new Class<?>[] { WebConfig.class };
-  }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
 
-  @Override
-  protected String[] getServletMappings() {
-    return new String[] { "/" };
-  }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 
-  @Override
-  protected Filter[] getServletFilters() {
-    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-    characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
-    return new Filter[] { characterEncodingFilter };
-  }
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+        return new Filter[] { characterEncodingFilter };
+    }
 }
